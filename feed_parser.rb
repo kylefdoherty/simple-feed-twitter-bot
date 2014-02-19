775845021f2fa875d5cbd23874d40248af7dbe55
Kyle Doherty <kyledoherty22@gmail.com>
2014-02-19 11:43:06 +0100

    added a proc to check if tweets are > 140, added the twitter gem back in, and put my keys and tokens in a config.yml file

--- feed_parser.rb
+++ feed_parser.rb
@@ -1,11 +1,36 @@
 require 'rss'
 require 'open-uri'
 require 'twitter'
+require 'yaml'
+
+
+
 
 class FeedParser 
 
 	def initialize(*feeds)
 		@feeds = feeds 
+		@tweet = ""
+		@pub_date = ""
+		@tweet_length = Proc.new{ 
+			if @tweet.length > 140
+				next 
+			else 
+				tweeter(@tweet)
+				puts "tweeted"
+
+				sleep(15)
+			end 
+		}
+
+		config = YAML.load_file('config.yml')
+
+		@client = Twitter::REST::Client.new({
+  			consumer_key: config['consumer_key'],
+  			consumer_secret: config['consumer_secret'],
+  			access_token: config['access_token'],
+  			access_token_secret: config['access_token_secret']
+		})
 		
 	end 
 
@@ -21,10 +46,8 @@ class FeedParser
 			
 				case 
 				when feed.feed_type == "rss"
-					puts "rss"
 					rss_parser(feed)
 				when feed.feed_type == "atom"
-					puts "atom"
 					atom_parser(feed) 
 				end 
 			end 
@@ -32,17 +55,29 @@ class FeedParser
 	end 
 
 	def rss_parser(rss)
-		rss.items.each {|item| puts "#{item.title} #{item.link}"}
+		rss.items.each do |item| 
+			title = item.title
+			link = item.link
+			@tweet = "#{title} #{link}"
+			@tweet_length.call 
+			
+		end 
 	end 
 
 
 	def atom_parser(atom)
 		atom.items.each do |item| 
-			puts "#{item.title.content} #{item.link.href}"
+			title = item.title.content
+			link = item.link.href
+			@tweet = "#{title} #{link}"
+			@tweet_length.call
+
 		end 
 	end 
 
-
+	def tweeter(tweet)
+		@client.update(tweet)
+	end 
 
 
 
@@ -52,6 +87,5 @@ end
 
 
 
-puts "hola"
 parser = FeedParser.new('https://gdata.youtube.com/feeds/api/users/goaztecscom/uploads', 'http://www.utsandiego.com/rss/headlines/sports/sdsu-aztecs/')
 parser.run
